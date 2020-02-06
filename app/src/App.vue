0659e26b640daf1946c71c1ba76b5df7d48a0e51
vicalle49@gmail.com <victor.delvalle@koalaboox.com>
2020-02-06 17:02:22 +0100

    add frontend app

---
+++ app/src/App.vue
@@ -0,0 +1,44 @@
+<template>
+  <div id="app">
+    <contract-component
+            :id="1"
+            :contract="contract1"
+    />
+    <btn>Trail Time!</btn>
+    <contract-component
+            :id="2"
+            :contract="contract2"
+    />
+  </div>
+</template>
+
+<script>
+import ContractComponent from './components/Contract'
+import Btn from "./components/Btn";
+import Contract from "./Domain/Contract";
+
+export default {
+  name: 'app',
+  components: {
+    "contract-component": ContractComponent,
+    Btn,
+  },
+  data: function() {
+    return {
+      contract1: new Contract(),
+      contract2: new Contract(),
+    }
+  },
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ app/src/Domain/Contract.js
@@ -0,0 +1,32 @@
+const rolePoints = {
+    K: 5,
+    N: 2,
+    V: 1,
+};
+
+const invalidators = {
+    V: 'K',
+};
+
+export default class Contract {
+    #roles = Array();
+
+    constructor(roles = '') {
+        this.#roles = roles;
+    }
+
+    set roles (roles) {
+        this.#roles = Array.from(roles);
+    }
+
+    get points () {
+        let points = 0;
+        let self = this;
+        (this.#roles || []).map(function (role) {
+            if (!(undefined !== typeof invalidators[role] && (self.#roles || []).includes(invalidators[role]))) {
+                points += rolePoints[role];
+            }
+        });
+        return points;
+    }
+}
